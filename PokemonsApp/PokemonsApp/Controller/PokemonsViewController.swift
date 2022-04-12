31729be34caa32a1d2200b7c932d6ef0dcf5e28c
Artur Drohobytskyy <artur.drohobytskyy@itsector.pt>
2022-04-12 10:07:54 +0100

    add: project structure

---
+++ PokemonsApp/PokemonsApp/Controller/PokemonsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonsApp
+//
+//  Created by @drohobytskyy on 12/04/2022.
+//
+
+import UIKit
+
+class PokemonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
